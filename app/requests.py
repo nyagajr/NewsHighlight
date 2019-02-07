ee4090458f8f01c9de1b121907461f12ba37bf44
Nyagajrr <muchokijnrbenson@gmail.com>
2019-02-07 15:03:37 +0300

    creating a function to process results

--- app/requests.py
+++ app/requests.py
@@ -36,3 +36,31 @@ def get_source(newss):
 
     return source_results
 
+
+def process_results(source_list):
+    '''
+    Function  that processes the source result and transforms them to a list of Objects
+
+    Args:
+        source_list: A list of dictionaries that contain source details
+
+    Returns :
+        source_results: A list of source objects
+    '''
+    source_results = []
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        language = source_item.get('language')
+        country = source_item.get('country')
+
+
+        if name:
+            source_object = Source(id, name, description, url, category, language, country)
+            source_results.append(source_object)
+
+    return source_results
+
