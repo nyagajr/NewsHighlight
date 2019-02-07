712e7abd64d24ac08be1dfde920b03973508c220
Nyagajrr <muchokijnrbenson@gmail.com>
2019-02-07 15:06:58 +0300

    configuring the apis

---
+++ config.py
@@ -0,0 +1,9 @@
+
+class Config:
+  '''
+  general configaration class
+  '''
+  NEWS_API_BASE_URL = 'https://newsapi.org/v2/{}?language=en&apiKey={}'
+  NEWS_API_KEY = '20a4ff7a91174704b2602d6d1300b154'
+  NEWS_API_BASE_URL2 = 'https://newsapi.org/v2/everything?q=apple&from=2019-02-06&to=2019-02-06&sortBy=popularity&apiKey={}'
+
