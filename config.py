23c73cd85aeb0d24494e7fdfae232061a88c25ae
Nyagajrr <muchokijnrbenson@gmail.com>
2019-02-07 15:07:26 +0300

    configuration

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
 
 class Config:
   '''
@@ -7,3 +8,21 @@ class Config:
   NEWS_API_KEY = '20a4ff7a91174704b2602d6d1300b154'
   NEWS_API_BASE_URL2 = 'https://newsapi.org/v2/everything?q=apple&from=2019-02-06&to=2019-02-06&sortBy=popularity&apiKey={}'
 
+
+class ProdConfig(Config):
+    '''
+    production configuration class
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    development configuration class
+    '''
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
