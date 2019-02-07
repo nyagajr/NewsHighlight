79ee88c946aab57fba3e4850dd88ee6a91a1b479
Nyagajrr <muchokijnrbenson@gmail.com>
2019-02-07 15:01:53 +0300

    getting the apis

--- app/requests.py
+++ app/requests.py
@@ -1,3 +1,12 @@
 import urllib.request, json
 from .models import Source, Article
 
+# getting api key
+api_key = None
+
+# getting the source base url
+base_url = None
+
+# getting the articles base url
+base_url2 = None
+
