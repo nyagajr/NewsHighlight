f28ab799826070138e26948bb8031e176a5fef0d
Nyagajrr <muchokijnrbenson@gmail.com>
2019-02-07 15:08:00 +0300

    testing using the unittest

---
+++ manage.py
@@ -0,0 +1,15 @@
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.command
+def test():
+    '''
+    Runs the unittest
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__=='__main__':
+    manager.run()
