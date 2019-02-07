f62b7fb62bfaa31a0113de603f5661b5a39e644e
Nyagajrr <muchokijnrbenson@gmail.com>
2019-02-07 15:02:47 +0300

    creating a function to get news

--- app/requests.py
+++ app/requests.py
@@ -16,3 +16,23 @@ def configure_request(app):
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
     base_url2 =app.config['NEWS_API_BASE_URL2']
+
+def get_source(newss):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = base_url.format(newss,api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results = None
+
+        if get_sources_response['sources']:
+            source_results_list = get_sources_response['sources']
+            source_results = process_results(source_results_list)
+
+
+    return source_results
+
